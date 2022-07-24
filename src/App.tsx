836c9adc83a2ac24437cf9e8a8030962e601c93b
John Doe <hamedhasan422421@gmail.com>
2022-07-24 20:54:16 +0300

    setup route

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Store from './pages/Store'
+import Navbar from './components/Navbar'
 
 function App() {
  
   return (
     <div className="App">
+      <Navbar/>
     <Container>
       <Routes>
         <Route path="/" element={<Home/>} />
