919bc8af02940fd3bf28375dc53468b809facee8
John Doe <hamedhasan422421@gmail.com>
2022-07-24 20:36:05 +0300

    setup all route with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Store from './pages/Store'
 
 function App() {
  
   return (
     <div className="App">
-    Hi
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/store" element={<Store/>} />
+        <Route path="/about" element={<About/>} />
+     
+      </Routes>
+    </Container>
     </div>
   )
 }
