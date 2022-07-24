64148e29664487fcc78127b224f3b1d456a8701a
John Doe <hamedhasan422421@gmail.com>
2022-07-24 22:38:08 +0300

    setup hooks uselocal storage

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,26 @@
+
+type StoreItemProps = {
+    id:number
+    name:string
+    price: number
+    imgUrl: string
+}
+
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+
+// const {
+//     getItemQuantity,
+//     increaseCartQuantity,
+//     decreaseCartQuantity,
+//     removeFromCart,
+// } = 
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default StoreItem;
